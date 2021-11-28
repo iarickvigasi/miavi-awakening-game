71f4d5bde3239fe1645d27fa5a17bb97b7d18ce0
Arick Vigas <arickvigas@Aricks-MacBook-Pro.local>
2021-11-28 12:34:38 +0200

    Initial commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    [SerializeField] Text mainText;
+    [SerializeField] State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainText.text = state.getText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = state.getNextStates()[0];
+            mainText.text = state.getText();
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = state.getNextStates()[1];
+            mainText.text = state.getText();
+        }
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10,14)][SerializeField] string text;
+    [SerializeField] State[] nextStates;
+
+    public string getText()
+    {
+        return text;
+    }
+
+    public State[] getNextStates()
+    {
+        return nextStates;
+    }
+}
